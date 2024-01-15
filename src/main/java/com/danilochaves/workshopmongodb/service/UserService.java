79df68d7cbde596397902da115708f83ce399460
DaniloChaves <danchavesds@gmail.com>
2024-01-15 19:42:11 -0300

    Implementação do findById

--- src/main/java/com/danilochaves/workshopmongodb/service/UserService.java
+++ src/main/java/com/danilochaves/workshopmongodb/service/UserService.java
@@ -2,10 +2,13 @@ package com.danilochaves.workshopmongodb.service;
 
 import com.danilochaves.workshopmongodb.entities.User;
 import com.danilochaves.workshopmongodb.repositories.UserRepository;
+import com.danilochaves.workshopmongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
@@ -14,5 +17,10 @@ public class UserService {
     public List<User> findAll(){
         List<User> list = repository.findAll();
         return list;
+    }
+
+    public User findById(String id){
+       Optional<User> obj = repository.findById(id);
+       return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
